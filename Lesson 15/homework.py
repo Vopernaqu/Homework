0fc17e8ee1654e8a77836df5ee43bad5c2a6d027
Vopernaqu <84228313+Vopernaqu@users.noreply.github.com>
2022-02-19 18:03:54 +0200

    Add files via upload

---
+++ Lesson 15/homework.py
@@ -0,0 +1,20 @@
+#1
+#def city_country(city, country):
+#    print(f"{city.title()} - {country.title()}")
+#i = 0
+#while i < 3 :
+#    city = input("Город")
+#    country = input("Страна")
+#    city_country(city, country)
+#    i += 1
+#2
+#albums = {}
+#def make_album(maker, name) :
+#    albums[maker] = name
+#    print(albums)
+#numbers = int(input("Сколько вы хотите альбомов"))
+#while numbers > 0 :
+#    maker = input("maker?")
+#    name = input("name?")
+#    make_album(maker, name)
+#    numbers -= 1
